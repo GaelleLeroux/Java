1b01480db6f71c50139b3b2db64ada7311caac01
Evann Nalewajek <evann.nalewajek@gmail.com>
2022-11-18 16:41:02 +0100

    Commit 1

--- src/tp/model/agents/Animal.java
+++ src/tp/model/agents/Animal.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 
 /**
  * Cette classe modélise un Animal dans la simulation
- * @author bruno
+ * @author Evann / Gaelle
  *
  */
 public class Animal {
@@ -19,19 +19,24 @@ public class Animal {
 	/** position sur la carte*/
 	private Point coord;
 	/** état de santé de l'animal */
-	private Etat etat=Etat.Normal;
+	private Etat etat;
 	private Sexe sexe;
 	
 	/* 
 	 * constructeurs 
 	 */
-	
+	/**
+	 * 
+	 * @param sexe
+	 * @param coord
+	 */
 	public Animal(Sexe sexe, Point coord) {
 		age = 0;
 		id = Animal.getUniqueId();
 		this.sexe=sexe;
 		//this.coord=coord;
 		this.coord=new Point(coord);
+		this.etat = Etat.Normal;
 	}
 	
 	public Animal(Sexe sexe) {
@@ -40,6 +45,11 @@ public class Animal {
 		 * une bonne manière de faire 
 		 * en utilisant ce qui existe déjà, une moins bonne
 		 */
+		age = 0;
+		this.sexe = sexe;
+		this.etat = Etat.Normal;
+		this.coord=new Point(0,0);
+		id = Animal.getUniqueId();
 	}
 	
 	public Animal() {
@@ -48,12 +58,61 @@ public class Animal {
 		 * une bonne manière de faire 
 		 * en utilisant ce qui existe déjà, une moins bonne
 		 */
+		age = 0;
+		this.sexe = sexe.Femelle;
+		this.coord=new Point(0,0);
+		id = Animal.getUniqueId();
+		this.etat = Etat.Normal;
 	}
 	
 	/*
 	 *  Accesseurs et mutateurs
 	 */
 	//TODO
+	/**
+	 * retourne l'âge
+	 * @return
+	 */
+	public int getAge() {
+		return this.age;
+	}
+	/**
+	 * retourne l'ID
+	 * @return
+	 */
+	public int getId() {
+		return this.id;
+	}
+	/**
+	 * retourne le sexe
+	 * @return
+	 */
+	public Sexe getSexe() {
+		return this.sexe; 
+	}
+	/**
+	 * retourne l'état
+	 * @return
+	 */
+	public Etat getEtat() {
+		return this.etat;
+	}
+	/**
+	 * retourne les coords
+	 * @return
+	 */
+	public Point getCoord() {
+		return new Point(this.coord.x, this.coord.y);
+	}
+	/**
+	 * Met à jour l'age si il veillit
+	 * @param new_age
+	 */
+	public void setAge(int new_age) {
+		if (new_age>age) {
+			age = new_age;
+		}
+	}
 	
 	
 	/*
@@ -75,6 +134,7 @@ public class Animal {
 	public void vieillir() {
 		//TODO fait vieillir l'animal d'une unité de temps
 		//une bonne manière de faire, une moins bonne...
+		setAge(getAge()+1);
 	}
 	
 	public void rencontrer(Animal a) {
@@ -87,7 +147,7 @@ public class Animal {
 	 * @return un identifiant entier unique d'animal
 	 */
 	private static int getUniqueId() {
-		//TODO 
+		//TODO  
 		return 0;
 	}
 	
@@ -97,7 +157,7 @@ public class Animal {
 		//complétez la méthode pour tester les nouvelles fonctionnalités que vous allez implémenter
 		Animal a = new Animal();
 		Animal b = new Animal(Sexe.Male);
-		//Animal c = new Animal(Sexe.Assexue);
+		Animal c = new Animal(Sexe.Assexue);
 		Animal d = new Animal(Sexe.Femelle,new Point(25,30));
 		Animal e = new Animal(Sexe.Femelle,new Point(25,30));
 		
@@ -109,35 +169,60 @@ public class Animal {
 		System.out.println(a);
 		System.out.println(a.toString());
 		System.out.println(b);
-		//System.out.println(c);
+		System.out.println(c);
 		System.out.println(d);
 		
 		System.out.println("*** Getters et setters **********");
-		/*
+		
+		System.out.println("\n-> GetSexe pour l'animal D");
 		System.out.println(d.getSexe());
 		Sexe ss = d.getSexe();
-		ss=Sexe.Male;
+		ss = Sexe.Male;
 		System.out.println(d.getSexe());
-		*/
-		//les lignes suivantes devraient afficher la même chose....
-		/*
+		
+		//les lignes suivantes devraient afficher la même chose...
+		
+		System.out.println("\n-> GetCoord pour l'animal D");
 		System.out.println(d.getCoord());
 		Point pt = d.getCoord();
-		pt.x=50;
+		pt.x = 50;
 		System.out.println(d.getCoord());
-		*/
+		
 		
 		//TODO ajoutez vos propres tests de getters et setters
 		
+		System.out.println("\n-> GetID pour l'animal D");
+		System.out.println(d.getId());
+		int idd = d.getId();
+		idd = 7145864;
+		System.out.println(d.getId());
+		
+		System.out.println("\n-> GetEtat pour l'animal D");
+		System.out.println(d.getEtat());
+		Etat eta = d.getEtat();
+		eta = Etat.PleineForme;
+		System.out.println(d.getEtat());
+		
+		System.out.println("\n-> GetAge pour l'animal A");
+		System.out.println(a.getAge());
+		
+		System.out.println("\n-> Test du setAge à 1");
+		a.setAge(1);
+		System.out.println(a.getAge());
+		int ag = a.getAge();
+		ag = 54;
+		System.out.println(a.getAge());
+		
+		
 		//TODO test vieillir
+		System.out.println("\n-> Test du vieillissement pour B");
+		System.out.println(b.getAge());
+		b.vieillir();
+		System.out.println(b.getAge());
 		
 		//TODO test seDeplacer
 		
 		//TODO test id
-		/*
-		System.out.println(a.getId());
-		System.out.println(b.getId());
-		*/
 		
 		/*
 		 * Test comparaison
